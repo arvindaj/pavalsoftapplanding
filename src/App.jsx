69e9675328dbe400576ef71791b5d573a1642619
arvindj <jeevaarvind489@gmail.com>
2025-07-04 14:58:47 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import './App.css'
-import HeroSection from './component/hero'
-import Features from './component/features'
-import MobileappSection from './component/moblie'
-import Services from './component/sevices'
-import Technologiespage from './component/technology'
-import Footer from './component/footer'
-import ContactSection from './component/contact'
+import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.css';
+
+import HeroSection from './component/hero';
+import Features from './component/features';
+import MobileappSection from './component/moblie';
+import Services from './component/sevices';
+import Technologiespage from './component/technology';
+import Footer from './component/footer';
+import ContactSection from './component/contact';
 
 function App() {
-
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to top on component mount
+  }, []);
 
   return (
     <>
       <HeroSection />
       <Features />
-      <MobileappSection/>
-      <Services/>
-      <Technologiespage/>
-      <ContactSection/>
-      <Footer/>
+      <MobileappSection />
+      <Services />
+      <Technologiespage />
+      <ContactSection />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
